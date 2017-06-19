c6121504c7203d6d381446532f6c3cf3027c6d7f
Savvanis Spyros <savvaniss@yahoo.gr>
2017-06-19 15:31:31 +0300

    force update before installation

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -6,14 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+#force to update before install
+include_recipe "apt"
+
+#install nginx package
 pachage 'nginx' do
  action :install
 end
 
+#enable and start nginx service
 service 'nginx' do
   action [ :enable, :start ]
 end
 
+#add template file to nginx server
 cookbook_file "/usr/share/nginx/www/index.html" do
   source "index.html"
   mode "0644"
