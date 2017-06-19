867dc00d095979f656b612f4b2b705f7e73d81ca
Savvanis Spyros <savvaniss@yahoo.gr>
2017-06-19 15:29:09 +0300

    adding instructions for nginx installation and apt-get update

---
+++ cookbooks/apt/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: apt
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+execute "apt-get updte" do
+ command "apt-get update"
+end
--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -6,3 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+pachage 'nginx' do
+ action :install
+end
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/usr/share/nginx/www/index.html" do
+  source "index.html"
+  mode "0644"
+end
