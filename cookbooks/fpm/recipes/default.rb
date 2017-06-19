82fd75b8deaaa93227f70c1363c5671a2a6fd436
Savvanis Spyros <savvaniss@yahoo.gr>
2017-06-19 16:14:26 +0300

    create default file for php-fpm isntallation

--- cookbooks/fpm/recipes/default.rb
+++ cookbooks/fpm/recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+#update system before installation
+include_recipe "apt"
+
+#install php-fpm
+multipackage_internal [ "php7.0-fpm", "php7.0-mysql", "php7.0-curl", "php7.0-json", "php7.0-intl", "php7.0-gd", "php7.0-mcrypt", "php7.0-xml", "php7.0-zip", "php7.0-mbstring", "php7.0-soap" ] do
+ action :install
+end
+
+#enable and start php-fpm service
+service 'php7.0-fpm' do
+  action [ :enable, :start ]
+end
+
+
