50821146c8efa82ada839012a2ad24c2b351a6dd
Savvanis Spyros <savvaniss@yahoo.gr>
2017-06-20 16:51:54 +0300

    fix symfony permitions

--- cookbooks/symfony/attributes/default.rb
+++ cookbooks/symfony/attributes/default.rb
@@ -1,3 +1,4 @@
 default['symfony']['apps_base_dir'] = '/var/www/html'
 default['symfony']['myapp']['branch'] = 'master'
 default['symfony']['app_name'] = 'app_name'
+default['symfony']['web_server']= 'www-data'
--- cookbooks/symfony/recipes/default.rb
+++ cookbooks/symfony/recipes/default.rb
@@ -32,3 +32,9 @@ end
 execute 'symfony_framework_install' do
   command "symfony new #{node['symfony']['apps_base_dir']}/#{node['symfony']['app_name']}" 
 end
+
+
+#change owner
+execute 'symfony_web_change_owner_group' do
+  command "chown -R #{node['symfony']['web_server']}:#{node['symfony']['web_server']}  #{node['symfony']['apps_base_dir']}"
+end
