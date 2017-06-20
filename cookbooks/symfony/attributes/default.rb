94fec812692e029a720ec3fdd67c36114abe2b0d
Savvanis Spyros <savvaniss@yahoo.gr>
2017-06-20 16:20:03 +0300

    add symfony command installation and symfony project creation based on attributes

--- cookbooks/symfony/attributes/default.rb
+++ cookbooks/symfony/attributes/default.rb
@@ -1,8 +1,3 @@
-default[:symfony][:ini]['memory_limit'] = '128M'
-default[:symfony][:ini]['date.timezone'] = 'UTC'
-default[:symfony][:console] = 'app/console'
-default[:symfony][:logs] = 'app/logs'
-default[:symfony][:cache] = 'app/cache'
-default[:symfony][:writable] = ['app/cache']
-default[:symfony][:composer_dev] = true
-default[:symfony][:composer_optimize_autoloader] = true
+default['symfony']['apps_base_dir'] = '/var/www/html'
+default['symfony']['myapp']['branch'] = 'master'
+default['symfony']['app_name'] = 'app_name'
--- cookbooks/symfony/recipes/default.rb
+++ cookbooks/symfony/recipes/default.rb
@@ -6,3 +6,29 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+#git "#{node['symfony']['app_base_dir']}" do
+#    repository "git@github.com:symfony/symfony-standard.git"
+#     revision node['symfony']['myapp']['branch']
+#    action :sync
+#end
+
+#create symfony installer
+
+execute 'symfony_make_bin' do
+  command 'mkdir -p /usr/local/bin'
+end
+
+execute 'symfony_get_installer' do
+  command 'curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony'
+
+end
+execute 'symfony_make_executable' do
+  command 'chmod a+x /usr/local/bin/symfony'
+end
+
+
+#install symfony in custom path
+execute 'symfony_framework_install' do
+  command "symfony new #{node['symfony']['apps_base_dir']}/#{node['symfony']['app_name']}" 
+end
