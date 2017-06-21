146e0d5f3f8554c4116c339bdd1f675fd8fffa63
Savvanis Spyros <savvaniss@yahoo.gr>
2017-06-21 12:12:21 +0300

    fix a symlink creation error

--- cookbooks/symfony/recipes/enable_vhosts.rb
+++ cookbooks/symfony/recipes/enable_vhosts.rb
@@ -1,10 +1,10 @@
 #symlink for port 80 redirect
-link '/etc/nginx/sites-enable/#{node['symfony']['vhost']}.conf' do
+link '/etc/nginx/sites-enable/' do
   to "/etc/nginx/sites-available/#{node['symfony']['vhost']}.conf"
 end
 
 #symlink for port 443 
-link '/etc/nginx/sites-enable/#{node['symfony']['vhost']}-ssl.conf' do
+link '/etc/nginx/sites-enable/' do
   to "/etc/nginx/sites-available/#{node['symfony']['vhost']}-ssl.conf"
 end
 
