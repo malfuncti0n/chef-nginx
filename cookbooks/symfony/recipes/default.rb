3e0d1c58429a8fdbf25dd4cbd49353aaf6aca24b
Savvanis Spyros <savvaniss@yahoo.gr>
2017-06-21 11:23:54 +0300

    minor changes for runtime of cookbooks

--- cookbooks/symfony/recipes/default.rb
+++ cookbooks/symfony/recipes/default.rb
@@ -13,6 +13,12 @@
 #    action :sync
 #end
 
+#the followings are cookbooks to run before symfony cookbook its self
+include_recipe "apt"
+include_recipe "chef_nginx"
+include_recipe "php-fpm"
+include_recipe "composer_c"
+#the following are recipes
 include_recipe "symfony::add_installer"
 include_recipe "symfony::new_symfony"
 include_recipe "symfony::create_vhosts"
